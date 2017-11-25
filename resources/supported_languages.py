b2d9effeaae72cf05b2ede46d531a4b1981ad9ad
Christian Bierschneider <christian.bierschneider@web.de>
2017-11-25 20:25:15 +0100

    Added file supported_languages.py, containing the correct abbreviation for different modes

---
+++ resources/supported_languages.py
@@ -0,0 +1,36 @@
+# Interactive and dictation mode
+interactive_dictation = {
+    'germany'        : 'de-DE',
+    'egypt'          : 'ar-EG',
+    'catalan'        : 'ca-ES',
+    'denmark'        : 'da-DK',
+    'australia'      : 'en-AU',
+    'canada'         : 'en-CA',
+    'united_kingdom' : 'en-GB',
+    'india'          : 'en-IN',
+    'new_zealand'    : 'en-NZ',
+    'united_states'  : 'en-US',
+    'spain'          : 'es-ES',
+    'mexico'         : 'es-MX',
+    'finland'        : 'fi-FI',
+    'canada_french'  : 'fr-CA',
+    'france'         : 'fr-FR',
+    'hindi'          : 'hi-IN',
+    'italy'          : 'it-IT',
+    'japan'          : 'ja-JP',
+    'korea'          : 'ko-KR',
+    'norway'         : 'nb-NO',
+    'netherlands'    : 'nl-NL',
+    'poland'         : 'pl-PL',
+    'brazil'         : 'pt-BR',
+    'portugal'       : 'pt-PT',
+    'russia'         : 'ru-RU',
+    'sweden'         : 'sv-SE',
+    'mandarin'       : 'zh-CN',
+    'hong_kong'      : 'zh-HK',
+    'taiwanese'      : 'zh-TW'
+}
+
+conversation = {
+
+}

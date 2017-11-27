4a4f833d6a0a33b8249eacd5c3ff2c6f1e3a32a9
Christian Bierschneider <christian.bierschneider@web.de>
2017-11-27 19:06:49 +0100

    Added new classes for the supported languages, to get the proper wav stream from TextToSpeech class

--- resources/supported_languages.py
+++ resources/supported_languages.py
@@ -34,3 +34,42 @@ interactive_dictation = {
 conversation = {
 
 }
+# https://docs.microsoft.com/en-us/azure/cognitive-services/speech/api-reference-rest/bingvoiceoutput
+
+
+class SupportedLocales:
+
+    def __init__(self, locale, gender, service_name_map):
+        self.locale = locale
+        self.gender = gender
+        self.service_name_map = service_name_map
+
+    def __str__(self):
+
+        return "%s: %s: %s" % (self.locale, self.gender, self.service_name_map)
+
+
+mode = [
+    SupportedLocales("de-DE", "Female", "Microsoft Server Speech Text to Speech Voice (de-DE, Hedda)"),
+    SupportedLocales("de-DE", "Male", "Microsoft Server Speech Text to Speech Voice (de-DE, Stefan, Apollo)"),
+    SupportedLocales("en-US", "Female", "Microsoft Server Speech Text to Speech Voice (en-US, ZiraRUS)"),
+    SupportedLocales("en-US", "Male", "Microsoft Server Speech Text to Speech Voice (en-US, BenjaminRUS)")
+]
+
+
+class LanguageCommand:
+
+    def __init__(self):
+        self.modes = [mode]
+
+    def __call__(self, locale, gender):
+
+        for m in self.modes:
+            for c in m:
+                if c.locale == locale and c.gender == gender:
+                    return c.service_name_map
+
+
+if __name__ == '__main__':
+   d = LanguageCommand()
+   print(d(locale='de-DE', gender='Male'))

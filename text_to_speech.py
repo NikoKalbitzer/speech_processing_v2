0c0160f02c221006a6a1bc91413ddf256f954411
Christian Bierschneider <christian.bierschneider@web.de>
2017-11-28 21:18:13 +0100

    improved class SpeechToText and class TextToSpeech to ensure faulty behavior

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,97 +1,113 @@
-import http.client, urllib.parse, json
+import http.client, urllib.parse
 from xml.etree import ElementTree
-from resources.supported_languages import TTSSupportedLocales, TTSLanguageCommand
+from resources.supported_languages import TTSLanguageCommand
 import pyaudio
-import wave
 
-lang = TTSLanguageCommand()
 
 
 class TextToSpeech:
 
-    def __init__(self):
+    def __init__(self, bing_key, language=None, gender=None, output_mode="riff-16khz-16bit-mono-pcm"):
+        """
+        constructor to initialize bing_key, language, gender and output mode
 
-        try:
-            with open('configs/configuration.json') as json_file:
-                json_data = json.load(json_file)
-                self.BING_KEY = json_data.get('Bing_Key')
-
-            self.accesstoken = self.get_access_token()
-
-        except FileNotFoundError as e:
-            print(str(e))
-        except KeyError as e:
-            print(str(e) + " is not defined in config.json")
-
-    def __del__(self):
+        :param bing_key: string, bing api key to get the access token
+        :param language: string, select appropriate language for the output speech
+        :param gender: string, gender of the speaker, can be 'Female' or 'Male'
+        :param output_mode: string,
         """
 
+        if isinstance(bing_key, str):
+            self.bing_key = bing_key
+        else:
+            raise TypeError("'bing_key' must be type of string")
 
+        self.access_token = self.__get_access_token()
+
+        if language is None:
+            self.language = 'germany'
+        else:
+            if isinstance(language, str):
+                self.language = language
+            else:
+                raise TypeError("'language' must be type of string")
+
+        if gender is None:
+            self.gender = 'Female'
+        else:
+            if isinstance(gender, str):
+                if (gender == 'Female') or (gender == 'Male'):
+                    self.gender = gender
+                else:
+                    raise ValueError("'gender' must be 'Female' or 'Male'")
+            else:
+                raise TypeError("'gender' must be type of string")
+
+        self.tts_lang = TTSLanguageCommand()
+
+        self.language_abbreviation, self.service_name_map = self.tts_lang(self.language, self.gender)
+
+        self.output_mode = output_mode
+
+    def __get_access_token(self):
         """
-        self.conn.close()
+        method to get the access token from Microsoft
 
-    def get_access_token(self):
+        :return: access_token
         """
 
-        :return:
-        """
-
-        headers = {"Ocp-Apim-Subscription-Key": self.BING_KEY}
+        headers = {"Ocp-Apim-Subscription-Key": self.bing_key}
         params = ""
         # AccessTokenUri = "https://api.cognitive.microsoft.com/sts/v1.0/issueToken";
-        AccessTokenHost = "api.cognitive.microsoft.com"
+        access_token_host = "api.cognitive.microsoft.com"
         path = "/sts/v1.0/issueToken"
 
         # Connect to server to get the Access Token
-        print("Connect to server to get the Access Token")
-        conn = http.client.HTTPSConnection(AccessTokenHost)
+        conn = http.client.HTTPSConnection(access_token_host)
         conn.request("POST", path, params, headers)
         response = conn.getresponse()
         if response.status == 200 and response.reason == 'OK':
             data = response.read()
             conn.close()
-            accesstoken = data.decode("UTF-8")
+            access_token = data.decode("UTF-8")
+            return access_token
         else:
             raise ValueError("Did not get the access token from microsoft bing")
 
-        return accesstoken
-
-    def request_to_bing(self, language='en-US', gender='Female', text=None, mode="riff-16khz-16bit-mono-pcm"):
+    def request_to_bing(self, text=None):
         """
+        method to request the Microsoft text to speech service
 
+        :param text, string, given text to speak
         :return:
         """
 
         body = ElementTree.Element('speak', version='1.0')
-        body.set('{http://www.w3.org/XML/1998/namespace}lang', language)
+        body.set('{http://www.w3.org/XML/1998/namespace}lang', self.language_abbreviation)
         voice = ElementTree.SubElement(body, 'voice')
-        voice.set('{http://www.w3.org/XML/1998/namespace}lang', language)
-        voice.set('{http://www.w3.org/XML/1998/namespace}gender', gender)
-        #voice.set('name', "Microsoft Server Speech Text to Speech Voice (en-US, ZiraRUS)")
-        service_name_map = lang(language, gender)
-        voice.set('name', service_name_map)
+        voice.set('{http://www.w3.org/XML/1998/namespace}lang', self.language_abbreviation)
+        voice.set('{http://www.w3.org/XML/1998/namespace}gender', self.gender)
+        voice.set('name', self.service_name_map)
         if text is None:
-            #voice.text = 'This is a demo to call microsoft text to speech service in Python.'
-            voice.text = 'now i will play music from david bowie'
+            voice.text = 'This is a demo to call microsoft text to speech service in Python.'
         else:
             if isinstance(text, str):
                 voice.text = text
             else:
-                raise TypeError("text must be a string object")
+                raise TypeError("text must be a type of string")
 
         headers = {"Content-type": "application/ssml+xml",
-                   "X-Microsoft-OutputFormat": mode,
-                   #"X-Microsoft-OutputFormat": "audio-16khz-128kbitrate-mono-mp3",
-                   "Authorization": "Bearer " + self.accesstoken,
+                   "X-Microsoft-OutputFormat": self.output_mode,
+                   "Authorization": "Bearer " + self.access_token,
                    "X-Search-AppId": "07D3234E49CE426DAA29772419F436CA",
                    "X-Search-ClientID": "1ECFAE91408841A480F00935DC390960",
                    "User-Agent": "TTSForPython"}
 
         # Connect to server to synthesize the wave
         print("\nConnect to server to get wav stream")
-        self.conn = http.client.HTTPSConnection("speech.platform.bing.com")
-        self.conn.request("POST", "/synthesize", ElementTree.tostring(body), headers)
-        response = self.conn.getresponse()
+        conn = http.client.HTTPSConnection("speech.platform.bing.com")
+        conn.request("POST", "/synthesize", ElementTree.tostring(body), headers)
+        response = conn.getresponse()
 
         if response.status == 200 and response.reason == 'OK':
             return response
@@ -126,14 +142,6 @@ class TextToSpeech:
             p.terminate()
         else:
             raise TypeError("http_response must be a HTTPResponse object")
-
-if __name__ == '__main__':
-
-    tts = TextToSpeech()
-    resp = tts.request_to_bing(language='en-US', gender='Female', text='Play music from David Bowie')
-    tts.play_request(resp)
-
-
 
 
 #data = response.read()

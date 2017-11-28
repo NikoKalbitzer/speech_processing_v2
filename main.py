bc73371b8e0e670fd819c76cc2093dadd3c09f28
Christian Bierschneider <christian.bierschneider@web.de>
2017-11-28 21:16:17 +0100

    updated main.py file with SpeechToText and TextToSpeech instance

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 import json
-
 from speech_to_text import SpeechToText
 from text_to_speech  import TextToSpeech
-
-try:
-    with open('configs/configuration.json') as json_file:
-        json_data = json.load(json_file)
-    BING_KEY = json_data.get('Bing_Key')
-except FileNotFoundError as e:
-    print(str(e))
-except KeyError as e:
-    print(str(e) + " is not defined in config.json")
 
 
 if __name__ == '__main__':
 
-    sst = SpeechToText(Bing_Key=BING_KEY, mode='interactive_dictation', language='germany')
-    sst_response = sst.start_recognize(recognizer='listen')
-    tts = TextToSpeech()
-    resp = tts.request_to_bing(language='de-DE', gender='Female', text=sst_response)
-    tts.play_request(resp)
+    try:
+
+        with open('configs/configuration.json') as json_file:
+            json_data = json.load(json_file)
+
+        BING_KEY = json_data.get('Bing_Key')
+
+        stt = SpeechToText(bing_key=BING_KEY, mode='interactive', language='germany')
+        stt_response = stt.start_recognize(recognizer='listen')
+
+        tts = TextToSpeech(bing_key=BING_KEY, language='germany', gender='Female')
+        resp = tts.request_to_bing(text=stt_response['DisplayText'])
+        tts.play_request(resp)
+
+    except FileNotFoundError as e:
+        print(str(e))
+    except KeyError as e:
+        print(str(e) + " is not defined in config.json")

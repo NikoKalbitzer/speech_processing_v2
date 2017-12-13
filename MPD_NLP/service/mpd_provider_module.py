e151c5e1518c53719e75633e364401509ca76ba6
Christian Bierschneider <christian.bierschneider@web.de>
2017-12-13 22:21:10 +0100

    Added the nlp files into the current project and deleted the flask server

---
+++ MPD_NLP/service/mpd_provider_module.py
@@ -0,0 +1,90 @@
+from termcolor import colored
+from MPD_NLP.service.response import Response, ErrorCodeEnum
+from random import randint
+from music_player.mpd_connection import ControlMPD
+color = "green"
+
+gernes = ["rock", "hard rock", "alternative", "electro house"]
+songs = [ "heroes" ]
+artists = [ "david bowie", "five finger death punch", "alan walker"]
+
+# not working for now - ConnectionRefusedError: [Errno 111] Connection refused
+mpdcontrol = ControlMPD("192.168.178.37", 6600)
+
+def playGerneSongArtist(arguments):
+    # determine if this chunks are gernes, artists or songs
+    # for gerne:
+    # should be only chunks with one gerne or <GERNE> + music
+    # if there are some gerne chunks and a artist, rather play the artist.
+    # if something unknown and a known gerne/artist/song is given, ignore the unknown
+    # if there is something unknown like 'very very hard rock' recursiveley remove the first? word and parse each argument
+    response = Response() # errorCode.Success and suggestion = None
+    arg_gernes = []
+    for chunk in arguments:
+        gerne = trimGerne(chunk)
+        if isGerne(gerne) == True:
+            arg_gernes.append(gerne)
+
+    if len(arguments) == 0:
+       playOrResume()
+    elif len(arg_gernes) < len(arguments) and containsSongOrArtist(arguments):
+       print(arguments)
+       print(colored("RESULT: playSongArtist(" + ", ".join(arguments) + ")", color))
+       songpos = mpdcontrol.match_in_database(arguments[0])
+       mpdcontrol.play(songpos)
+    elif len(arg_gernes) > 0:
+       playGernes(arg_gernes)
+    else:
+        # no gerne song artist found, check for alternate suggestions
+        response.errorCode = ErrorCodeEnum.ParsingError
+        # TODO: suggest a song / gerne / artist depending
+        response.suggestion = gernes[randint(0, len(gernes)-1)]
+
+    return response
+
+def isGerne(gerne):
+    if trimGerne(gerne).lower() in gernes:
+        return True;
+    return False;
+
+def trimGerne(gerne):
+    # cut ' music' in the end
+    music = "music"
+    if gerne.lower().endswith(music):
+        gerne = gerne[:len(gerne)-(len(music)+1)]
+    return gerne
+
+def playGernes(gernes):
+    print(colored("RESULT: playGernes(" + ", ".join(gernes) + ")", color))
+
+def containsSongOrArtist(arguments):
+    for argument in arguments:
+        if isArtist(argument) or isSong(argument):
+            return True
+    return False
+
+def isArtist(argument):
+    return argument.lower() in artists
+
+def isSong(argument):
+    return argument.lower() in songs
+
+def stop():
+    print(colored("RESULT: stop()", color))
+    mpdcontrol.stop()
+
+def pause():
+    print(colored("RESULT: pause()", color))
+
+def resume():
+    print(colored("RESULT: resume()", color))
+
+def playOrResume():
+    print(colored("RESULT: playOrResume()", color))
+
+def playRandom():
+    print(colored("RESULT: playRandom()", color))
+
+def playNext():
+    print(colored("RESULT: playNext()", color))
+    mpdcontrol.next()
---
+++ MPD_NLP/service/parse.py
@@ -0,0 +1,159 @@
+import spacy
+from termcolor import colored
+import MPD_NLP.service.mpd_provider_module as mpm
+from MPD_NLP.service import verbalizer
+from enum import Enum
+from expiringdict import ExpiringDict
+from MPD_NLP.service.conversationState import ConversationStateEnum, ConversationState
+from MPD_NLP.service.response import Response, ErrorCodeEnum
+from flask import Flask, request
+app = Flask(__name__)
+
+nlp = spacy.load("en_core_web_lg")
+
+
+# conversation state is stored in a expiringdict
+# note that there an additional state which is also the initial state which is considered if no state is stored
+
+states = ExpiringDict(max_len=100, max_age_seconds=10)
+# states[userid] = ConversationState(ConversationStateEnum.AwaitYesOrNo, "Ask for David Bowie?")
+
+
+print("READY for requests")
+
+
+
+@app.route("/")
+def parseREST():
+    input = request.args.get('input')
+    userid = request.args.get('userid')
+    print("REQUEST from id " + userid + ": " + input)
+    return parse(input, userid)
+
+
+def parse(input, userid):
+    #start with part of speech tagging
+    doc = nlp(input)
+    print(doc)
+    response = "initial value aka no instruction found."
+
+    if states.get(userid) == None:
+        for token in doc:
+            if token.lemma_ == "play":
+
+                print("PLAY instruction found")
+                # check if there is a negation
+                if is_negative(token) != True:
+                    if token.nbor().lemma_ == "next":
+                        response = playNext()
+                    elif token.nbor().lemma_ == "random":
+                        response = playRandom()
+                    elif token.nbor().lemma_ == "a" and token.nbor().nbor().lemma == "random":
+                        response = playRandom()
+                    elif token.nbor().lemma_ == "something":
+                        # ask for a artist/songname or gerne
+                        states[userid] = ConversationState(ConversationStateEnum.AwaitSongArtistOrGerne)
+                        return verbalizer.getQuestionForArtistSongGerneOrRandom()
+                    else:
+                        response = play(doc, userid)
+                else:
+                    # input is something like: Don't play David Bowie.
+                    response = verbalizer.getDontPlayText()
+                break
+            elif token.lemma_ == "stop":
+                print("STOP instruction found")
+                if is_negative(token) != True:
+                    response = stop()
+                else:
+                    # input is something like: Don't stop.
+                    response = verbalizer.getDontStopPauseText()
+                break
+            elif token.lemma_ == "pause":
+                print("PAUSE instruction found")
+                if is_negative(token) != True:
+                    response = pause()
+                else:
+                    # input is something like: Don't pause.
+                    response = verbalizer.getDontStopPauseText()
+                break
+            elif token.lemma_ == "resume" or token.lemma_ == "continue":
+                print("RESUME instruction found")
+                if is_negative(token) != True:
+                    response = resume()
+                else:
+                    # input is something like: Don't resume.
+                    response = verbalizer.getDontResumeText()
+                break
+            elif token.lemma_ == "next" and len(doc) <= 2:
+                print("NEXT instruction found")
+                response = playNext()
+                break
+
+    elif states.get(userid).state == ConversationStateEnum.AwaitYesOrNo:
+        print("Yes or no")
+        state = states.pop(userid) # remove state
+        if doc[0].lemma_ == "yes":
+            return parse(state.suggestion, userid) # simply call with a suggestion like 'Play rock.'
+        else:
+            return "Oh, ok."
+    elif states.get(userid).state == ConversationStateEnum.AwaitSongArtistOrGerne:
+        print("Song, Gerne or Artist")
+        states.pop(userid) # remove state
+        return parse("Play " + str(doc), userid)
+
+    return ">> " + response + "\n"
+
+def is_negative(token):
+    # if there is a negation for play, it is a children of play in the graph
+    for child in token.children:
+        #print(child.text + " " + child.dep_)
+        if child.dep_ == "neg":
+            print("NEG found")
+            return True
+    return False
+
+def play(doc, userid):
+    chunks = list(doc.noun_chunks)
+    # determine if this chunks are gernes, artists or songs
+    # for gerne:
+    # should be only one chunk with one word or <GERNE> + music
+    print("CHUNKS: " + str(chunks))
+
+    arguments = []
+    for chunk in chunks:
+        arguments.append(str(chunk))
+
+    response = mpm.playGerneSongArtist(arguments)
+
+    if response.errorCode == ErrorCodeEnum.Success:
+        return verbalizer.getOkText()
+    elif response.errorCode == ErrorCodeEnum.ParsingError:
+        states[userid] = ConversationState(ConversationStateEnum.AwaitYesOrNo, "Play " + response.suggestion + ".")
+        return verbalizer.getAlternatePlaySuggestion(response.suggestion)
+    elif response.errorCode == ErrorCodeEnum.ConnectionError:
+        return verbalizer.getConnectionError()
+
+def stop():
+    mpm.stop()
+    return verbalizer.getOkText()
+
+def pause():
+    mpm.pause()
+    return verbalizer.getOkText()
+
+def resume():
+    mpm.resume()
+    return verbalizer.getOkText()
+
+def playNext():
+    mpm.playNext()
+    return verbalizer.getOkText()
+
+def playRandom():
+    mpm.playRandom()
+    return verbalizer.getOkText()
+
+
+if __name__ == '__main__':
+    resp = parse("play next song", 1)
+    #print(resp)

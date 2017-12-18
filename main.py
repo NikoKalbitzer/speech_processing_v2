07d9ea31d7b9c53a2f4fbb9c5f4411d87c6846ad
Christian_laptop <christian.bierschneider@web.de>
2017-12-18 13:50:22 +0100

    Deleted unused description in the main file

--- main.py
+++ main.py
@@ -12,27 +12,9 @@ if __name__ == '__main__':
         BING_KEY = json_data.get('Bing_Key')
 
         stt = SpeechToText(bing_key=BING_KEY, mode='interactive', language='united_states')
-        #mpdclient = ControlMPD(json_data.get('MPD_Server'), json_data.get('MPD_Port'))
-
 
         stt_response = stt.start_recognize(recognizer='listen_in_background')
-        #str_for_mpd = stt_response['DisplayText'].strip(".").split(" ")
-        #print(str_for_mpd)
-        #mpdclient.clear_current_playlist()
-        #print(mpdclient.get_current_song_playlist())
-        #mpdclient.update_database()
-        """
-        try:
-            songpos = mpdclient.match_in_database(str_for_mpd[1])
-            print(songpos)
-            mpdclient.play(songpos)
-            sleep(10)
-            mpdclient.stop()
-        except (IndexError, CommandError):
-            tts = TextToSpeech(bing_key=BING_KEY, language='united_states', gender='Female')
-            resp = tts.request_to_bing(text='Sry i did not understand!')
-            tts.play_request(resp)
-        """
+
     except FileNotFoundError as e:
         print(str(e))
     except KeyError as e:

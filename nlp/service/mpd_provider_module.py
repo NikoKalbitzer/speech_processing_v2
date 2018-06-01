88da76ef374e41b299689dcc834962f875f6f342
Christian Bierschneider <christian.bierschneider@web.de>
2018-06-01 15:48:36 +0200

    Added statement string.title() for better mpd database results

--- nlp/service/mpd_provider_module.py
+++ nlp/service/mpd_provider_module.py
@@ -18,6 +18,7 @@ except ConnectionRefusedError:
     print(colored("ConnectionRefusedError: check if your MPD server is running!", "red"))
     exit(0)
 
+
 def trimGerne(gerne):
     # cut ' music' in the end
     music = "music"
@@ -25,20 +26,28 @@ def trimGerne(gerne):
         gerne = gerne[:len(gerne)-(len(music)+1)]
     return gerne
 
+
 def containsSongOrArtist(arguments):
     for argument in arguments:
+        argument = argument.title()
         if mpdcontrol.is_artist_in_db(argument) or mpdcontrol.is_title_in_db(argument):
             return True
     return False
 
+
 def playSongOrArtist(arguments):
     print(colored("RESULT: playSongOrArtist(" + ", ".join(arguments) + ")", color))
     for i in arguments:
+        i = i.title()
+        print(i)
         song_pos = mpdcontrol.add_artist_to_pl(i)
+        if song_pos is None:
+            song_pos = mpdcontrol.add_title_to_pl(i)
         if song_pos is not None:
             mpdcontrol.play(song_pos)
             print(mpdcontrol.get_current_song_playlist())
             sleep(10)
+
 
 def isGerne(gerne):
     gerne = trimGerne(gerne).lower()
@@ -47,12 +56,14 @@ def isGerne(gerne):
     else:
         return False
 
+
 def getRandomGenre():
     print("getRandomGenre")
     #genres = mpdcontrol.get_all_genres_in_db()
 
     gernes = ["rock", "hard rock", "alternative", "electro house"]
     return gernes[randint(0, len(gernes)-1)]
+
 
 # gernes is a list of gernes f. e. ['rock', 'electro house'] or ['rock']
 def playGernes(gernes):
@@ -63,20 +74,25 @@ def playGernes(gernes):
         print(mpdcontrol.get_current_song_playlist())
         sleep(10)
 
+
 def stop():
     print(colored("RESULT: stop()", color))
     mpdcontrol.stop()
+
 
 def pause():
     print(colored("RESULT: pause()", color))
     mpdcontrol.pause()
 
+
 def resume():
     print(colored("RESULT: resume()", color))
     mpdcontrol.play()
 
+
 def playOrResume():
     print(colored("RESULT: playOrResume()", color))
+
 
 def playRandom():
     print(colored("RESULT: playRandom()", color))
@@ -84,28 +100,35 @@ def playRandom():
     mpdcontrol.set_random()
     mpdcontrol.play()
 
+
 def playNext():
     print(colored("RESULT: playNext()", color))
     mpdcontrol.next()
+
 
 def playPrevious():
     print(colored("RESULT: playPrevious()", color))
     mpdcontrol.previous()
 
+
 def clearCurrentPlaylist():
     print(colored("RESULT: clearCurrentPlaylist()", color))
     mpdcontrol.clear_current_playlist()
+
 
 def repeatPlaylist():
     print(colored("RESULT: repeatPlaylist()", color))
     mpdcontrol.set_repeat()
 
+
 def repeatSong():
     print(colored("RESULT: repeatSong()", color))
+
 
 def updateDatabase():
     print(colored("RESULT: updateDatabase()", color))
     mpdcontrol.update_database()
+
 
 def speak(message):
     ## BING_KEY not known
